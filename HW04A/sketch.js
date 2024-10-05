95a359bef930a400c5eabdc1f21afc7711db3d5e
xingyu yao <xy2779@nyu.edu>
2024-10-04 22:53:23 -0400

    Update sketch.js

--- HW04A/sketch.js
+++ HW04A/sketch.js
@@ -1,5 +1,28 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(240,240,240);
+  noFill();
+
+
+
+
+
+  // draw a column of rows
+  for (let y = 10; y < windowHeight; y += 30) {
+    // draw a row of squares
+    for (let x = 10; x < windowWidth; x += 30) {
+      let rr = random(255);
+      let rg = random(255);
+      let rb = random(255);
+    
+      stroke(rr, rg, rb);
+      rect(x, y,20,20,2,2,2,2);
+      let c = random(10);
+      strokeWeight(c);
+
+    }
+  }
+
 }
 
 function draw() {
